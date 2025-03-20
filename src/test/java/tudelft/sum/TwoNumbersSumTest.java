4c6b562ed77639bda44ba44472fa3d1ea5fc6e75
AndresCT1 <carlos.calvo@ucsm.edu.pe>
2025-03-20 00:03:59 -0500

    chore: Correción del bug del ejercicio NumFinder

--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,15 @@
 package tudelft.sum;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 public class TwoNumbersSumTest {
+    public static void main(String[] args) {
+        TwoNumbersSum sumCalculator = new TwoNumbersSum();
 
+        ArrayList<Integer> num1 = new ArrayList<>(Arrays.asList(2, 4, 3));
+        ArrayList<Integer> num2 = new ArrayList<>(Arrays.asList(5, 6, 4));
+
+        ArrayList<Integer> result = sumCalculator.addTwoNumbers(num1, num2);
+
+        System.out.println("Suma: " + result);
+    }
 }
